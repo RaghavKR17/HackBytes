1be9917596e61c1cad6fce6e00f94b88ed4ee0d8
DevanshiR2157 <110126370+DevanshiR2157@users.noreply.github.com>
2024-04-21 14:03:42 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Volunteer Opportunity Finder! Feel free to use this anytime!");
@@ -11,7 +9,7 @@ public class Main {
         // Example usage of the application
         VolunteerOpportunityFinder app = new VolunteerOpportunityFinder(city);
         app.run();
-        
+         
         scanner.close();
     }
 }
