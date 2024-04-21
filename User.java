f5fc98cd59e026a7e96485e9eb475dbcf93c2f93
DevanshiR2157 <110126370+DevanshiR2157@users.noreply.github.com>
2024-04-21 14:49:21 -0700

    Updated User.java

--- User.java
+++ User.java
@@ -1,12 +1,12 @@
 public class User {
     private String username;
     private String password;
-    private UserProfile profile; // New field for user profile
+    private UserProfile profile; // UserProfile object for the user
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.profile = new UserProfile();
+        this.profile = new UserProfile(username); // Initialize UserProfile with the username
     }
 
     public String getUsername() {
@@ -17,11 +17,7 @@ public class User {
         return password;
     }
 
-    public boolean authenticate(String username, String password) {
-        return this.username.equals(username) && this.password.equals(password);
-    }
-
-    // Getter for user profile
+    // Getter for UserProfile
     public UserProfile getProfile() {
         return profile;
     }
