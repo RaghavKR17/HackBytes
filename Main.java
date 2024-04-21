324a930e07e52f9160e390f0bd2772310f2a7716
DevanshiR2157 <110126370+DevanshiR2157@users.noreply.github.com>
2024-04-21 14:48:08 -0700

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Volunteer Opportunity Finder! Feel free to use this anytime!");
